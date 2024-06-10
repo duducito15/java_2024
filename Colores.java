d08dfd8a58591899ee925e22bf813cc00dae6abf
Eduardo <echavez@utea.edu.pe>
2024-06-10 09:31:13 -0500

    Inicio de Java

---
+++ Colores.java
@@ -0,0 +1,7 @@
+public class Colores {
+    public static void main(String[] args) {
+        System.out.println("\033[33m mandarina");
+        System.out.println("\033[32m hierba");
+        System.out.println("\033[31m tomate");
+    }
+}
