bc92175daf9cb5c6110f9690cd237c60ecf4a842
Eduardo <echavez@utea.edu.pe>
2024-06-17 09:06:50 -0500

    Variables

---
+++ Semana3/Variables.java
@@ -0,0 +1,25 @@
+package Semana3;
+
+public class Variables {
+    public static void main(String[] args) {
+        /*
+         * Variables
+         * es un contenedor de información, es como una cajita
+         * en la cual se peude introducir un valor estos pueden ser
+         * numeros enteros, numeros con decimales, caracteres, cadena
+         * de caracteres, el valor de la variable puede cambiar durante
+         * la ejecucion del programa.
+         * La variables tiene nombres que los representa, y es fuertemente
+         * tipado eso significa que se deben declarar.
+         */
+        // Tipos de datos
+        // Entero (int y long)
+
+        int numero1;
+        numero1 = 10;
+        System.out.println("El valor actual es: " + numero1);
+
+        numero1 = 20;
+        System.out.println("y ahora es: " + numero1);
+    }
+}
