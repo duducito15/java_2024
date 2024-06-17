ead4b8d3a7de44188577ffc66fc00c003b3e33c7
Eduardo <echavez@utea.edu.pe>
2024-06-17 09:34:55 -0500

    Uso de String

---
+++ Semana3/UsoDeStrings.java
@@ -0,0 +1,11 @@
+package Semana3;
+public class UsoDeStrings {
+    public static void main(String[] args) {
+        String palabra = "Cerveza";
+        String frase = "¿dónde esta mi cerveza?";
+
+        System.out.println("Palabra que uso con frecuencia: "+palabra);
+        System.out.println("Una frase que uso a veces: "+frase);
+        
+    }
+}
