affc8b0944e940d9053748b2b1325d034b23fb47
Eduardo <echavez@utea.edu.pe>
2024-06-17 09:16:31 -0500

    Con decimales

---
+++ Semana3/VariablesConDecimales.java
@@ -0,0 +1,16 @@
+package Semana3;
+
+public class VariablesConDecimales {
+    public static void main(String[] args) {
+        // Variables numericas con decimales (double y float)
+        double promedio1;
+        promedio1 = 12.75;
+
+        double promedio2;
+        promedio2 = 13;
+
+        System.out.println(promedio1);
+        System.out.println(promedio2);
+    }
+
+}
