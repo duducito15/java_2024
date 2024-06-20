6e236fff0686077e3b0211f6f7aa3befde27c7fb
Eduardo <echavez@utea.edu.pe>
2024-06-20 12:29:44 -0500

    LeeNumeros

---
+++ Semana3/Ejercicios/Ejercicio1.java
@@ -0,0 +1,8 @@
+package Semana3.Ejercicios;
+
+public class Ejercicio1 {
+    public static void main(String[] args) {
+        //Realizar un conversor de euros a soles, la cantidad de euros que se quiere convertir 
+        // debe ser ingresada por teclado.
+    }
+}
---
+++ Semana3/LeeNumeros.java
@@ -0,0 +1,29 @@
+package Semana3;
+public class LeeNumeros {
+    public static void main(String[] args) {
+        String linea;
+        //Entrada
+        System.out.println("Ingresa el primer número: ");
+        linea = System.console().readLine();
+        int numero1;
+        numero1 = Integer.parseInt(linea);
+
+        System.out.println("Ingresa el segundo número: ");
+        linea = System.console().readLine();
+        int numero2;
+        numero2 = Integer.parseInt(linea);
+        //Proceso
+        int suma = numero1 + numero2;
+        int resta = numero1 - numero2;
+        int multiplicacion = numero1 * numero2;
+        double division = (double) numero1 / (double) numero2;
+        //Salida
+        System.out.println("La suma es: " + suma);
+        System.out.println("La resta es: " + resta);
+        System.out.println("La multiplicacion es: " + multiplicacion);
+        System.out.println("La division es: " + division);
+/*
+ * Ejercicio 1: 
+ */
+    }
+}
