b105ae1a0be315b345cd29786dbe5e25cb74cec5
Eduardo <echavez@utea.edu.pe>
2024-07-01 08:14:10 -0500

    Semana 5 inicio

--- Semana3/LeerDatosScanner.java
+++ Semana3/LeerDatosScanner.java
@@ -7,11 +7,9 @@ public class LeerDatosScanner {
         //La Clases Scanner
         Scanner lee = new Scanner(System.in);
 
-        System.out.println("Introduce tu nombre: ");
-        String nombre = lee.nextLine();
-
-        System.out.println("Introduce tu edad: ");
-        int edad = Integer.parseInt( lee.nextLine());
+        System.out.print("Introduce tu nombre y edad separados por un espacio: ");
+        String nombre = lee.next();
+        int edad = lee.nextInt();
 
         System.out.println("Tu nombre es: "+ nombre +", tu edad es: "+edad);
 
---
+++ Semana5/EstrcuturasCondicionales.java
@@ -0,0 +1,7 @@
+package Semana5;
+
+public class EstrcuturasCondicionales {
+    public static void main(String[] args) {
+        
+    }
+}
