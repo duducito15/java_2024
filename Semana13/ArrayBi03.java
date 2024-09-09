cbef4103ee2f9c81cbfee9da592556f2ab6d10aa
Dudu <echavezv@utea.edu.pe>
2024-09-09 09:08:25 -0500

    EjerArrayBi02 completo

--- Semana13/ArrayBi03.java
+++ Semana13/ArrayBi03.java
@@ -3,23 +3,10 @@ package Semana13;
 public class ArrayBi03 {
     public static void main(String[] args) {
 
-        int[][] n = new int[3][2];
-
-        int f, c;
-
-        for (f = 0; f < 3; f++) {
-            for (c = 0; c < 2; c++) {
-                n[f][c] = (int)(Math.random()*100+0);
-            }
-        }
-
-        for ( f = 0; f < 3 ; f++) { // para las filas
-            System.out.print("Fila "+f+": ");
-
-            for (c = 0; c < 2; c++) { // para las columnas
-                System.out.printf("%10d", n[f][c]);                
-            }
-            System.out.println();
-         }
+        double soles, euros;
+        
+        soles = 500.0;
+        euros = soles / 4.18;
+        System.out.println(euros);
     }
 }
