6d8072d2034273ab59890d1201fd149ad9b93fb4
Eduardo <echavezv@utea.edu.pe>
2024-09-19 12:13:07 -0500

    Funcion Primo completa

--- Semana14/Funcion1.java
+++ Semana14/Funcion1.java
@@ -6,18 +6,20 @@ public class Funcion1 {
         System.out.println("Ingrese un número entero positivo: ");
         int n = Integer.parseInt(System.console().readLine());
 
-        boolean esPrimo = true;
+        if (esPrimo(n)) {
+            System.out.println("El " + n + " es primo");
+        } else {
+            System.out.println("El " + n + " NO es primo");
+        }
+    }
 
-        for (int i = 2; i < n; i++) {
-            if ((n % i) == 0) {
-                esPrimo = false;
+    public static boolean esPrimo(int x) {
+
+        for (int i = 2; i < x; i++) {
+            if ((x % i) == 0) {
+                return false;
             }
         }
-
-        if (esPrimo) {
-            System.out.println("El "+ n + " es primo");
-        }else{
-            System.out.println("El "+ n + " NO es primo");
-        }
+        return true;
     }
 }
